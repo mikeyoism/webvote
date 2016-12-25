f9afbada46378da5009b8e46afb3fdb0263c24e5
Staffan Ulfberg <staffanu@multivac.madeye.org>
2016-12-26 00:59:33 +0100

    Upgraded to bootstrap 4alpha5. Added vote code entry. Removed star rating plugin (for performance reasons).

--- list/list.js
+++ list/list.js
@@ -20,6 +20,8 @@ var beer_db = function()
     var user_votes = null;
 
     var current_popup_item_id = null;
+
+    var vote_code = null;
     
     function init()
     {
@@ -54,6 +56,20 @@ var beer_db = function()
 		user_votes = user_data[competition_id].votes;
 
 		initialize_html();
+
+		// Now that the competition information is known we can handle
+		// vote code changes.
+		$('#vote-code').on('keyup', function(e) {
+		    var code = $(this).val();
+		    if (code.length > 6)
+		    {
+			code = code.substr(0, 6);
+			$(this).val(code);
+		    }
+		    var code = $(this).val();
+		    update_vote_code(code);
+		    return false;
+		});
 	    },
 	    error: function(xhr, status, thrown) {
 		alert("error: " + status + xhr + thrown);
@@ -66,19 +82,21 @@ var beer_db = function()
 	// Add items to the nav bar class selection dropdown.
 	var class_dropdown = $('ul #class-dropdown');
 	$.each(classes, function(i, vote_class) {
-	    class_dropdown.append('<li><a data-toggle="tab" href="#page-' + vote_class.id
+	    class_dropdown.append('<li class="dropdown-item"><a data-toggle="tab" href="#page-' + vote_class.id
 				  + '" id="menu-item-' + vote_class.id + '">' + vote_class.name + '</a></li>');
 	});
 
+	/*
 	// Register events on the class selections to update the button text.
 	$("#class-dropdown li a").click(function() {
-	    $('#class-dropdown-button').html($(this).text() + ' <span class="caret"></span>');
+	    $('#class-dropdown-button').html($(this).text());
 	});
 
 	// Clear current class name on class selection button when moving to the voting page
 	$("#vote-menu-button").click(function() {
-	    $('#class-dropdown-button').html("Ölklasser " + ' <span class="caret"></span>');
+	    $('#class-dropdown-button').html("Ölklasser");
 	});
+	*/
 
 	// Default to the first class (this only sets the pill text -- by default the first tab is selected.
 	$('#menu-item-' + classes[0].id).trigger('click');
@@ -95,7 +113,7 @@ var beer_db = function()
 
 	$("#popup").on('hidden.bs.modal', function(event) {
 	    var comment = $("#popup-comment").val();
-	    var rating = $("#popup-rating").rating('rate');
+	    var rating = $("input[type='radio'][name='popup-rating']:checked").val()
 	    var medal = $("input[type='radio'][name='popup-medal']:checked").val();
 
 	    set_medal(current_popup_item_id, medal);
@@ -106,10 +124,19 @@ var beer_db = function()
 	    };
 	    localStorage.setItem('user_data', JSON.stringify(user_data));
 
-	    $('#rating-display-' + current_popup_item_id).rating('rate', rating);
+	    update_rating_in_beer_list(current_popup_item_id, rating);
 	});
 	
 	fill_beer_lists(compare_beers_by_entry_id, '#page-' + classes[0].id);
+
+	// Workaround for tabs not being inactivated coorectly.
+	// Remove when bootstrap fixes this (hopefully in 4alpha6).
+	$('.nav-item').on('shown.bs.tab', 'a', function (e) {
+	    if (e.relatedTarget) {
+		$(e.relatedTarget).removeClass('active');
+	    }
+	})
+	
 	fill_vote_form();
     }
 
@@ -153,13 +180,18 @@ var beer_db = function()
 	    var beer = beers[entry_id];
 	    var class_id = beer['class'];
 
+	    var rating = 0;
+	    if (entry_id in user_beers) {
+		rating = user_beers[entry_id].rating;
+	    }
+
 	    items[class_id] = items[class_id] || [];
 	    items[class_id].push(
-		'<a class="list-group-item" id="' + entry_id + '" href="#" data-toggle="modal" data-target="#popup">'
-		    + '<span class="pull-right"><input type="hidden" id="rating-display-' + entry_id
-		    + '"     class="rating" data-readonly data-filled="glyphicon glyphicon-star gold"' 
-		    + '      data-empty="glyphicon glyphicon-star grey"/></span>'
-		    + '<span class="pull-right" id="medal-display-' + entry_id + '" style="padding-right: 10px;"></span>'
+		'<a class="list-group-item list-group-item-action" id="' + entry_id + '" href="#" data-toggle="modal" data-target="#popup">'
+		    + '<span class="float-xs-right" id="rating-display-' + entry_id + '">'
+		    + get_rating_string(rating)
+		    + '</span>'
+		    + '<span class="float-xs-right" id="medal-display-' + entry_id + '" style="padding-right: 10px;"></span>'
 		    + '<span class="beer-number">' + beer.beerCounter + '</span>. '
 		    + '<span class="beer-name">' + beer.BeerName + '</span><br>'
 		    + '<span class="beer-style">' + beer.styleName + ' (' + beer.styleId + ')</span>'
@@ -171,8 +203,11 @@ var beer_db = function()
 	$.each(classes, function(i, vote_class) {
 	    $('#page-' + vote_class.id).remove();
 	    
-	    pages.push('<div id="page-' + vote_class.id + '" class="tab-pane fade ' 
-		       + ('#page-' + vote_class.id == active_tab_hash ? ' in active' : '') + '">');
+	    pages.push('<div id="page-' + vote_class.id + '" class="tab-pane ' 
+		       + ('#page-' + vote_class.id == active_tab_hash ? ' active' : '') + '">');
+
+	    pages.push('<h1 class="display-4">' + vote_class.name + '</h1>');
+	    
 	    pages.push('<div id="beerlist-' + vote_class.id + '" class="list-group">');
 	    pages.push(items[vote_class.id].join(''));
 	    pages.push('</div>');
@@ -192,18 +227,14 @@ var beer_db = function()
 	    });
 	});
 
-	// Display the rating and maybe a medal for each item
+	// Maybe display a medal for some items (do not call if 0 -- we just created the
+	// list so there are no medals shown by default.
 	$.each(sorted_beers, function(i, entry_id) {
-	    var rating = 0;
 	    var medal = 0;
 	    if (entry_id in user_beers) {
-		rating = user_beers[entry_id].rating;
 		medal = get_medal(entry_id);
+		update_medal_in_beer_list(entry_id, medal);
 	    }
-	    var rating_input = $('#rating-display-' + entry_id);
-	    rating_input.rating('rate', rating);
-
-	    update_medal_in_beer_list(entry_id, medal);
 	});
     }
 
@@ -212,11 +243,11 @@ var beer_db = function()
 	var items = [];
 	
 	$.each(classes, function(i, vote_class) {
-	    items.push('<div class="row">');
-	    items.push('<div class="col-xs-1"><label>' + vote_class.name + '</label></div>');
+	    items.push('<div class="form-group row">');
+	    items.push('  <div class="col-xs-3"><label>' + vote_class.name + '</label></div>');
 	    for (var j = 1; j <= 3; j++)
 	    {
-		items.push('<div class="col-xs-1"><input type="text" class="form-control" id="vote-form-'
+		items.push('<div class="col-xs-3"><input type="text" class="form-control" id="vote-form-'
 			   + vote_class.id + "-" + j + '" value="');
 		if (user_votes[vote_class.id][j])
 		{
@@ -254,29 +285,47 @@ var beer_db = function()
 	var medal = get_medal(item_id);
 
 	$("#popup-comment").val(comment);
-	$("#popup-rating").rating('rate', rating);
 	$("#popup-medal-" + medal + "-button").button('toggle');
+
+	$('input[name="popup-rating"][value="' + rating + '"').prop('checked', true);
 	
 	current_popup_item_id = item_id;
     }
     
-    function update_medal_in_beer_list(entry_id, medal) {
+    function update_rating_in_beer_list(entry_id, rating)
+    {
+	var rating_span = $('#rating-display-' + entry_id);
+	rating_span.html(get_rating_string(rating));
+    }
+
+    function get_rating_string(rating)
+    {
+	return '<span class="gold">' + '&#9733;'.repeat(rating) + '</span>'
+	    + '<span class="grey">' + '&#9734;'.repeat(5 - rating) + '</span>';
+	// return '<span class="fa fa-star fa-fw gold"></span>'.repeat(rating)
+	//    + '<span class="fa fa-star fa-fw grey"></span>'.repeat(5 - rating);
+    }
+
+    function update_medal_in_beer_list(entry_id, medal)
+    {
 	var medal_span = $('#medal-display-' + entry_id);
-	if (medal == 1 || medal == 2 || medal == 3) {
+	if (medal == 0) {
+	    medal_span.html("");
+	} else {
 	    if (medal == 1) {
 		var value_str = 'Guld';
 		var color = 'gold';
 	    } else if (medal == 2) {
 		var value_str = 'Silver';
 		var color = 'silver';
-	    } else {
+	    } else if (medal == 3) {
 		var value_str = 'Brons';
 		var color = '#cd7f32';
+	    } else {
+		alert('error medal value=' + medal);
 	    }
-	    medal_span.html('<span class="label" style="background-color: ' + color + ';">'
+	    medal_span.html('<span class="tag" style="background-color: ' + color + ';">'
 			    + value_str + "</label>");
-	} else {
-	    medal_span.html("");
 	}
     }
 
@@ -299,7 +348,7 @@ var beer_db = function()
 
 	$.each(user_votes[class_id], function(i, beer) {
 	    if (i == medal && beer != entry_id
-		|| medal == 0 && beer == entry_id) {
+		|| i != medal && beer == entry_id) {
 		delete user_votes[class_id][i];
 		update_medal_in_beer_list(beer, 0);
 		$('#vote-form-' + class_id + '-' + i).val('');
@@ -317,7 +366,7 @@ var beer_db = function()
     // Otherwise, return the empty string.
     function get_current_tab_hash()
     {
-	var as = $("ul#class-dropdown li.active").find('a');
+	var as = $("ul#class-dropdown a.active");
 	if (as.length == 1)
 	{
 	    return as[0].hash;
@@ -327,8 +376,83 @@ var beer_db = function()
 	    return '';
 	}
     }
+
+    function update_vote_code(code)
+    {
+	vote_code = code;
+	
+	var input_field = $('#vote-code');
+	var form_group = input_field.closest('.form-group');
+	input_field.removeClass('form-control-success');
+	input_field.removeClass('form-control-warning');
+	input_field.removeClass('form-control-danger');
+	form_group.removeClass('has-success');
+	form_group.removeClass('has-warning');
+	form_group.removeClass('has-danger');
+	if (code.length == 6)
+	{
+            read_votes();
+	}
+	else if (code.length != 0)
+	{
+	    form_group.addClass('has-warning');
+	    input_field.addClass('form-control-warning');
+	}
+    }
+
+    function read_votes()
+    {
+        $.ajax({  
+            type: "POST",
+            url: "/vote/ajax/vote.php",  
+	    contentType: 'application/json',
+            dataType: 'json',
+            cache : false,
+            data: JSON.stringify({
+                vote_code: vote_code
+            }),
+
+            success: function(response) {
+		var input_field = $('#vote-code');
+		var form_group = input_field.closest('.form-group');
+                if ("vote_code" in response)
+		{
+		    vote_code = response.vote_code; // uppercased etc
+                    input_field.val(vote_code);
+		    form_group.addClass('has-success');
+		    input_field.addClass('form-control-success');
+                    //if (supportsHtml5Storage())
+		    //{
+                    //    localStorage['vote_code'] = response.vote_code;
+		    //}
+                }
+		else
+		{
+		    form_group.addClass('has-danger');
+		    input_field.addClass('form-control-danger');
+		}
+
+		/*
+                $.each(sys_cat, function(index, value)
+		{
+                    for (i = 1; i <= VOTES_PER_CAT; i++)
+		    {
+                        $('#' + value + '_vote' + i).prop("disabled", disableInput); // prevent input on bad vote code
+
+			var vote = "votes" in response ? response.votes[value][i] : '';
+                        $('#' + value + '_vote' + i).val(vote);
+                    }
+                });
+		*/
+            },
+            error: function(xhr, textStatus, errorThrown)
+	    {
+		alert("error: " + textStatus + ", responseText: " + xhr.responseText); 
+            }  
+        });
+    }
     
     return {
-	init : init
+	init: init
     }
 }();
